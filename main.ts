88e3319d9dbcae02d0f3a00ba5a9f8babff1ef76
Jacob Carpenter <github@jacobcarpenter.com>
2020-05-28 22:23:20 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,555 @@
+namespace SpriteKind {
+    export const ScheduledAction = SpriteKind.create()
+}
+namespace myTiles {
+    //% blockIdentity=images._tile
+    export const tile0 = img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
+    //% blockIdentity=images._tile
+    export const tile1 = img`
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 6 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 6 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 6 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 6 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 6 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+`
+    //% blockIdentity=images._tile
+    export const tile3 = img`
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 6 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 6 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 6 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+`
+    //% blockIdentity=images._tile
+    export const tile5 = img`
+d d d d d d d e d d d d d d d b 
+d e e e e e e e d d e e e e e e 
+d d d d d d d d d d d d d d d d 
+b d d d d d d d d d d b d d d d 
+d d d d d d d d d d d d d d d d 
+e e e e e e e e e e e e e e d d 
+b d d d d d d d d d d d d d e d 
+d d d d d d d d d d d d d d d d 
+d d d d b d d d d d d d d d d d 
+d d e e e e e e e e e e e e e d 
+d d d d d d d d d d d d d d d e 
+d d d d d d d d d d d d d d d d 
+d d d b d d d d d d d d d d d b 
+d e e e e e e e e e e e e e e d 
+d d d d d d d d d d d d d d d d 
+b d d d d d d d d d d d d d d d 
+`
+    //% blockIdentity=images._tile
+    export const tile6 = img`
+d d d d d d d e d d d d d d d b 
+d e e e 7 e e 7 d d e e e 7 e e 
+d d d d d d d d d d d d d d d d 
+b d d d d d d d d d d b d d d d 
+d d 7 d d 7 d d d d d 7 d d d d 
+e e 7 e e e 7 e e e 7 e e 7 d d 
+b d d d d d d d d d d d d d e d 
+d d d d d d d d d d d d d d d d 
+d d d d 7 d d d d d d 7 d d d d 
+d d e e e 7 e e e e 7 e e e e d 
+d d d d d d d d d d d d d d d e 
+d d d d d d d d d d d d d d d d 
+d d 7 b d d d d 7 d d d 7 d d b 
+d e 7 e e e e 7 e e e e 7 e e d 
+d d d d d d d d d d d d d d d d 
+b d d d d d d d d d d d d d d d 
+`
+    //% blockIdentity=images._tile
+    export const tile7 = img`
+d d d d 7 d d 7 d d d d d 7 d b 
+d e e e 7 e e 7 d d e e e 7 e e 
+d d d d d d d d d d d d d d d d 
+b d 7 d 7 7 d d d d d 7 d d 7 d 
+d d 7 d d 7 d d d d 7 7 d 7 7 d 
+e e 7 e e e 7 e e e 7 e e 7 d d 
+b d d d d d d d d d d d d d e d 
+d d d d 7 d d d d d d 7 d d d d 
+d d d d 7 7 d d d d 7 7 d d d d 
+d d e e e 7 e e e e 7 e e e e d 
+d d d d d d d d d d d d d d d e 
+d d 7 d d d d d 7 d d d d 7 d d 
+d d 7 b d d d 7 7 d d d 7 7 d b 
+d e 7 e e e e 7 e e e e 7 e e d 
+d d d d d d d d d d d d d d d d 
+b d d d d d d d d d d d d d d d 
+`
+    //% blockIdentity=images._tile
+    export const tile8 = img`
+d d d 5 7 d d 7 d d d d 5 7 d b 
+d e 5 e 7 e 5 7 d d 5 7 7 7 5 7 
+d 7 5 d d d 5 d d d 5 d 5 d 5 d 
+b d 6 d 7 6 6 d d d d 6 d d 6 d 
+d d 7 d d 6 d d d d 7 7 d 7 6 d 
+e 7 7 5 e 5 7 e 5 e 7 e 6 7 d d 
+b d d 5 d 5 d d 5 d d 5 6 d 7 d 
+d d d 5 6 d d d d 6 d 5 d d 5 d 
+d 5 d d 6 7 d 5 d 5 7 7 5 d 5 d 
+d 5 7 6 e 7 e 5 e 7 7 e 5 e 7 d 
+d 6 d 7 d d d 6 d 7 d d 6 d 7 e 
+d 6 6 d d d d d 6 7 d d d 6 d d 
+d d 6 7 7 d d 6 6 d d d 6 6 d b 
+d e 7 e e 7 7 6 7 e 7 e 7 7 7 d 
+d d d d d d d d d d d d d d d d 
+b d d d d d d d d d d d d d d d 
+`
+    //% blockIdentity=images._tile
+    export const tile9 = img`
+d d d a 7 d d 7 d d d d a 7 d b 
+d e a e 7 e a 7 d d a 7 7 7 a 7 
+d 7 a d d d a d d d a d a d a d 
+b d 6 d 7 6 6 d d d d 6 d d 6 d 
+d d 7 a d 6 d d d d 7 7 d 7 6 d 
+e 7 7 a e a 7 e a e 7 e 6 7 d d 
+b d d a d a d d a d d a 6 d 7 d 
+d d d a 6 d d d d 6 d a d d a d 
+d a d d 6 7 d a d a 7 7 a d a a 
+a a 7 6 e 7 a a e 7 7 e a e 7 d 
+d 6 d 7 d d d 6 d 7 d d 6 d 7 e 
+d 6 6 d d d d d 6 7 d d d 6 d d 
+d d 6 7 7 d d 6 6 d d d 6 6 d b 
+d e 7 e e 7 7 6 7 e 7 e 7 7 7 d 
+d d d d d d d d d d d d d d d d 
+b d d d d d d d d d d d d d d d 
+`
+    //% blockIdentity=images._tile
+    export const tile10 = img`
+d d d 2 7 d d 7 d d d d 2 7 d b 
+d e 2 e 7 e 2 7 d d 2 7 7 7 d 7 
+d 7 2 d d d 2 d d d 2 d 2 d 2 d 
+b d 6 d 7 6 6 d d d d 6 d d 6 d 
+d d 7 d d 6 d d d d 7 7 d 7 6 d 
+e 7 7 e e 2 7 e b e 7 e 6 7 d d 
+b d d 2 d 2 d d 2 d d 2 6 d 7 d 
+d d d 2 6 d d d d 6 d 2 d d d d 
+d d d d 6 7 d 2 d 2 7 7 2 d 2 d 
+d 2 7 6 e 7 e 2 e 7 7 e 2 e 7 d 
+d 6 d 7 d d d 6 d 7 d d 6 d 7 e 
+d 6 6 d d d d d 6 7 d d d 6 d d 
+d d 6 7 7 d d 6 6 d d d 6 6 d b 
+d e 7 e e 7 7 6 7 e 7 e 7 7 7 d 
+d d d d d d d d d d d d d d d d 
+b d d d d d d d d d d d d d d d 
+`
+    //% blockIdentity=images._tile
+    export const tile11 = img`
+d d d 8 7 d d 7 d d d d 8 7 d b 
+d 8 8 e 7 e 8 7 d d 8 7 7 7 8 7 
+d 7 8 d d d 8 8 d d 8 d 8 d 8 d 
+b d 6 d 7 6 6 d d d d 6 d d 6 d 
+d d 7 d d 6 d d d d 7 7 d 7 6 d 
+e 7 7 8 e 8 7 e 8 8 7 e 6 7 d d 
+b d 8 8 d 8 d d 8 d 8 8 6 d 7 d 
+d d d 8 6 d d d d 6 d 8 d d 8 d 
+d 8 d d 6 7 d 8 d 8 7 7 8 d 8 d 
+8 8 7 6 e 7 e 8 8 7 7 e 8 8 7 d 
+d 6 d 7 d d d 6 d 7 d d 6 d 7 e 
+d 6 6 d d d d d 6 7 d d d 6 d d 
+d d 6 7 7 d d 6 6 d d d 6 6 d b 
+d e 7 e e 7 7 6 7 e 7 e 7 7 7 d 
+d d d d d d d d d d d d d d d d 
+b d d d d d d d d d d d d d d d 
+`
+}
+function updateActionIcon (scheduledAction: Sprite) {
+    if (showTaskIcons) {
+        scheduledAction.setImage(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`)
+    } else {
+        scheduledAction.setImage(actionIcons[sprites.readDataNumber(scheduledAction, "action")])
+    }
+}
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    changeMode()
+})
+controller.up.onEvent(ControllerButtonEvent.Repeated, function () {
+    moveCursor(0, -1)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    scheduleAction()
+})
+controller.right.onEvent(ControllerButtonEvent.Repeated, function () {
+    moveCursor(1, 0)
+})
+function getScheduledActionAtSpriteLocation (target: Sprite) {
+    targetTilemapLocation = getTilemapLocationForSprite(target)
+    for (let value of sprites.allOfKind(SpriteKind.ScheduledAction)) {
+        tilemapLocationForScheduledAction = getTilemapLocationForSprite(value)
+        if (tilemap.locationXY(targetTilemapLocation, tilemap.XY.column) == tilemap.locationXY(tilemapLocationForScheduledAction, tilemap.XY.column) && tilemap.locationXY(targetTilemapLocation, tilemap.XY.row) == tilemap.locationXY(tilemapLocationForScheduledAction, tilemap.XY.row)) {
+            return value
+        }
+    }
+    // kind of awkward, but a return value is required
+    return target
+}
+controller.down.onEvent(ControllerButtonEvent.Repeated, function () {
+    moveCursor(0, 1)
+})
+function getTilemapLocationForSprite (s: Sprite) {
+    return tiles.getTileLocation(Math.idiv(s.x, 16), Math.idiv(s.y, 16))
+}
+function findNextTask () {
+    if (workerStatus != 0) {
+        return
+    }
+    workerTilemapLocation = getTilemapLocationForSprite(worker)
+    shortestPath = []
+    for (let value of sprites.allOfKind(SpriteKind.ScheduledAction)) {
+        potentialNextTaskPath = scene.aStar(workerTilemapLocation, getTilemapLocationForSprite(value))
+        if (shortestPath.length == 0 || potentialNextTaskPath.length < shortestPath.length) {
+            workerStatus = 1
+            shortestPath = potentialNextTaskPath
+            taskTarget = value
+        }
+    }
+    if (workerStatus == 1) {
+        scene.followPath(worker, shortestPath, 40)
+    }
+}
+function moveCursor (xChange: number, yChange: number) {
+    xPos = Math.constrain(xPos + xChange, 0, tilemap.tilemapColumns() - 1)
+    yPos = Math.constrain(yPos + yChange, 0, tilemap.tilemapRows() - 1)
+    animation.runMovementAnimation(
+    cursor,
+    "L" + (Math.imul(xPos, 16) + 8) + "," + (Math.imul(yPos, 16) + 8),
+    100,
+    false
+    )
+}
+function changeMode () {
+    mode = (mode + 1) % modeIcons.length
+    cursor.setImage(modeIcons[mode])
+}
+function cancelCurrentScheduledAction () {
+    if (workerStatus == 1) {
+        workerStatus = 0
+        scene.followPath(worker, [tilemap.locationOfSprite(worker)], 40)
+    }
+}
+function scheduleAction () {
+    cancelCurrentScheduledAction()
+    spriteAtLocation = getScheduledActionAtSpriteLocation(cursor)
+    if (!(canScheduleAction())) {
+        return
+    }
+    if (mode != 0 && spriteAtLocation.kind() != SpriteKind.ScheduledAction) {
+        scheduled = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, SpriteKind.ScheduledAction)
+        sprites.setDataNumber(scheduled, "action", mode)
+        updateActionIcon(scheduled)
+        tiles.placeOnTile(scheduled, tiles.getTileLocation(xPos, yPos))
+    } else if (mode == 0 && spriteAtLocation.kind() == SpriteKind.ScheduledAction) {
+        spriteAtLocation.destroy()
+    }
+    findNextTask()
+}
+function canScheduleAction () {
+    if (tilemap.tileIsWall(tiles.getTileLocation(xPos, yPos))) {
+        return false
+    }
+    if (mode == 1) {
+        return tilemap.tileIs(tiles.getTileLocation(xPos, yPos), myTiles.tile1) || tilemap.tileIs(tiles.getTileLocation(xPos, yPos), myTiles.tile3) || (tilemap.tileIs(tiles.getTileLocation(xPos, yPos), myTiles.tile8) || tilemap.tileIs(tiles.getTileLocation(xPos, yPos), myTiles.tile9) || (tilemap.tileIs(tiles.getTileLocation(xPos, yPos), myTiles.tile10) || tilemap.tileIs(tiles.getTileLocation(xPos, yPos), myTiles.tile11)))
+    }
+    if (mode == 2) {
+        return tilemap.tileIs(tiles.getTileLocation(xPos, yPos), myTiles.tile5)
+    }
+    return mode == 0
+}
+controller.left.onEvent(ControllerButtonEvent.Repeated, function () {
+    moveCursor(-1, 0)
+})
+let completingTaskId = 0
+let fullGrownFlowerType = 0
+let scheduled: Sprite = null
+let spriteAtLocation: Sprite = null
+let taskTarget: Sprite = null
+let potentialNextTaskPath: tiles.Location[] = []
+let shortestPath: tiles.Location[] = []
+let workerTilemapLocation: tiles.Location = null
+let tilemapLocationForScheduledAction: tiles.Location = null
+let targetTilemapLocation: tiles.Location = null
+let yPos = 0
+let xPos = 0
+let cursor: Sprite = null
+let workerStatus = 0
+let worker: Sprite = null
+let mode = 0
+let actionIcons: Image[] = []
+let modeIcons: Image[] = []
+let showTaskIcons = false
+controller.configureRepeatEventDefaults(0, 240)
+showTaskIcons = false
+modeIcons = [img`
+5 5 5 5 5 . . . . . . 5 5 5 5 5 
+5 5 5 5 5 . . . . . . 5 5 5 5 5 
+5 5 . . . . . . . . . . . . 5 5 
+5 5 . . . . . . . . . . . . 5 5 
+5 5 . . . . . . . . . . . . 5 5 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+5 5 . . . . . . . . . . . . 5 5 
+5 5 . . . . . . . . . . . . 5 5 
+5 5 . . . . . . . . . . . . 5 5 
+5 5 5 5 5 . . . . . . 5 5 5 5 5 
+5 5 5 5 5 . . . . . . 5 5 5 5 5 
+`, img`
+5 5 5 5 5 . . . . . . 5 5 5 5 5 
+5 5 5 5 5 . . . . . . 5 5 5 5 5 
+5 5 . . . . . . . . . . . . 5 5 
+5 5 . . . . 6 6 6 6 6 6 6 . 5 5 
+5 5 . . . 6 6 6 6 6 6 6 6 . 5 5 
+. . . . . . . . . . 6 6 . . . . 
+. . . . . . . . . 6 6 . . . . . 
+. . . . . . . . 6 6 . . . . . . 
+. . . . . . . 6 6 . . . . . . . 
+. . . . . . 6 6 . . . . . . . . 
+. . . . . 6 6 . . . . . . . . . 
+5 5 . . 6 6 . . . . . . . . 5 5 
+5 5 . 6 6 . . . . . . . . . 5 5 
+5 5 . . . . . . . . . . . . 5 5 
+5 5 5 5 5 . . . . . . 5 5 5 5 5 
+5 5 5 5 5 . . . . . . 5 5 5 5 5 
+`, img`
+5 5 5 5 5 . . . . . . 5 5 5 5 5 
+5 5 5 5 5 . . . . . . 5 5 5 5 5 
+5 5 . . . . . . . . . . . . 5 5 
+5 5 . . . . . . . . . . . . 5 5 
+5 5 . . 6 6 6 6 6 . . . . . 5 5 
+. . . . 6 6 6 6 6 6 . . . . . . 
+. . . . 6 6 6 6 6 6 6 . . . . . 
+. . . . 6 6 6 6 6 6 6 . . . . . 
+. . . . 6 6 6 6 6 6 6 6 . . . . 
+. . . . . 6 6 6 6 6 6 6 . . . . 
+. . . . . . 6 6 6 6 6 6 . . . . 
+5 5 . . . . . . . . 6 6 6 . 5 5 
+5 5 . . . . . . . . . . 6 . 5 5 
+5 5 . . . . . . . . . . . . 5 5 
+5 5 5 5 5 . . . . . . 5 5 5 5 5 
+5 5 5 5 5 . . . . . . 5 5 5 5 5 
+`]
+actionIcons = [img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
+. 1 6 6 6 6 6 6 6 6 6 6 6 6 1 . 
+. 1 6 6 6 6 1 1 1 1 1 1 1 6 1 . 
+. 1 6 6 6 1 1 1 1 1 1 1 1 6 1 . 
+. 1 6 6 6 6 6 6 6 6 1 1 6 6 1 . 
+. 1 6 6 6 6 6 6 6 1 1 6 6 6 1 . 
+. 1 6 6 6 6 6 6 1 1 6 6 6 6 1 . 
+. 1 6 6 6 6 6 1 1 6 6 6 6 6 1 . 
+. 1 6 6 6 6 1 1 6 6 6 6 6 6 1 . 
+. 1 6 6 6 1 1 6 6 6 6 6 6 6 1 . 
+. 1 6 6 1 1 6 6 6 6 6 6 6 6 1 . 
+. 1 6 1 1 6 6 6 6 6 6 6 6 6 1 . 
+. 1 6 6 6 6 6 6 6 6 6 6 6 6 1 . 
+. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
+. . . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
+. 1 6 6 6 6 6 6 6 6 6 6 6 6 1 . 
+. 1 6 6 6 6 6 6 6 6 6 6 6 6 1 . 
+. 1 6 6 1 1 1 1 1 6 6 6 6 6 1 . 
+. 1 6 6 1 1 1 1 1 1 6 6 6 6 1 . 
+. 1 6 6 1 1 1 1 1 1 1 6 6 6 1 . 
+. 1 6 6 1 1 1 1 1 1 1 6 6 6 1 . 
+. 1 6 6 1 1 1 1 1 1 1 1 6 6 1 . 
+. 1 6 6 6 1 1 1 1 1 1 1 6 6 1 . 
+. 1 6 6 6 6 1 1 1 1 1 1 6 6 1 . 
+. 1 6 6 6 6 6 6 6 6 1 1 1 6 1 . 
+. 1 6 6 6 6 6 6 6 6 6 6 1 6 1 . 
+. 1 6 6 6 6 6 6 6 6 6 6 6 6 1 . 
+. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
+. . . . . . . . . . . . . . . . 
+`]
+mode = 1
+scene.setBackgroundColor(9)
+tiles.setTilemap(tiles.createTilemap(
+            hex`0a0008000000000000000000000001010101010101010101020202010101020102020101020201010101010201010101010101010102010201020202020102010101020201020202010101010101010201020101`,
+            img`
+2 2 2 2 2 2 2 2 2 2 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+`,
+            [myTiles.tile0,myTiles.tile1,myTiles.tile3,myTiles.tile5,myTiles.tile6,myTiles.tile7,myTiles.tile8,myTiles.tile9,myTiles.tile10,myTiles.tile11],
+            TileScale.Sixteen
+        ))
+worker = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . c c c . . . . . . . 
+. . . . . c c c c c . . . . . . 
+. . . . . . c c c . . . . . . . 
+. . . . . . . c . . . . . . . . 
+. . . . . . c c c . . . . . . . 
+. . . . . c . c . c . . . . . . 
+. . . . . . . c . . . . . . . . 
+. . . . . . c . c . . . . . . . 
+. . . . . . c . c . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, SpriteKind.Player)
+workerStatus = 0
+tiles.placeOnTile(worker, tiles.getTileLocation(8, 2))
+cursor = sprites.create(modeIcons[mode], SpriteKind.Player)
+cursor.setFlag(SpriteFlag.Ghost, true)
+scene.cameraFollowSprite(cursor)
+xPos = 1
+yPos = 2
+cursor.left = Math.imul(xPos, 16)
+cursor.top = Math.imul(yPos, 16)
+game.onUpdateInterval(5000, function () {
+    for (let value of tiles.getTilesByType(myTiles.tile7)) {
+        if (Math.percentChance(20)) {
+            fullGrownFlowerType = randint(0, 3)
+            if (fullGrownFlowerType == 0) {
+                tiles.setTileAt(value, myTiles.tile8)
+            } else if (fullGrownFlowerType == 1) {
+                tiles.setTileAt(value, myTiles.tile9)
+            } else if (fullGrownFlowerType == 2) {
+                tiles.setTileAt(value, myTiles.tile10)
+            } else if (fullGrownFlowerType == 3) {
+                tiles.setTileAt(value, myTiles.tile11)
+            }
+        }
+    }
+    for (let value of tiles.getTilesByType(myTiles.tile6)) {
+        if (Math.percentChance(25)) {
+            tiles.setTileAt(value, myTiles.tile7)
+        }
+    }
+})
+forever(function () {
+    pause(1000)
+    if (workerStatus == 0) {
+        findNextTask()
+        if (workerStatus == 0 && Math.percentChance(8)) {
+            scene.followPath(worker, scene.aStar(getTilemapLocationForSprite(worker), tiles.getTileLocation(randint(1, tilemap.tilemapColumns() - 2), randint(1, tilemap.tilemapRows() - 1))), 40)
+        }
+    } else if (workerStatus == 1) {
+        if (getScheduledActionAtSpriteLocation(worker) == taskTarget) {
+            workerStatus = 2
+            completingTaskId = sprites.readDataNumber(taskTarget, "action")
+            taskTarget.destroy()
+            worker.say("working!")
+            pause(500)
+            worker.say("")
+            workerTilemapLocation = getTilemapLocationForSprite(worker)
+            if (completingTaskId == 1) {
+                tiles.setTileAt(workerTilemapLocation, myTiles.tile5)
+            } else if (completingTaskId == 2) {
+                tiles.setTileAt(workerTilemapLocation, myTiles.tile6)
+            }
+            workerStatus = 0
+        }
+    }
+})
+game.onUpdateInterval(500, function () {
+    showTaskIcons = !(showTaskIcons)
+    for (let value of sprites.allOfKind(SpriteKind.ScheduledAction)) {
+        updateActionIcon(value)
+    }
+})
